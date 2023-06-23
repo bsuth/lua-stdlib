5c3c108e9ecc170feff867696558669f7abe58be
bsuth <bsuth701@gmail.com>
2023-06-23 15:31:01 +0200

    restructure project

---
+++ stdlib.lua
@@ -0,0 +1,273 @@
+local _MODULE = {}
+local function load()
+	for key, value in pairs(_MODULE) do
+		local value_type = type(value)
+		if value_type == "function" then
+			if key ~= "load" and key ~= "unload" then
+				_G[key] = value
+			end
+		elseif value_type == "table" then
+			local library = _G[key]
+			if type(library) == "table" then
+				for subkey, subvalue in pairs(value) do
+					library[subkey] = subvalue
+				end
+			end
+		end
+	end
+end
+_MODULE.load = load
+local function unload()
+	for key, value in pairs(_MODULE) do
+		local value_type = type(value)
+		if value_type == "function" then
+			if _G[key] == value then
+				_G[key] = nil
+			end
+		elseif value_type == "table" then
+			local library = _G[key]
+			if type(library) == "table" then
+				for subkey, subvalue in pairs(value) do
+					if library[subkey] == subvalue then
+						library[subkey] = nil
+					end
+				end
+			end
+		end
+	end
+end
+_MODULE.unload = unload
+local function _kpairs_iter(a, i)
+	local key, value = i, nil
+	repeat
+		key, value = next(a, key)
+	until type(key) ~= "number"
+	return key, value
+end
+local function kpairs(t)
+	return _kpairs_iter, t, nil
+end
+_MODULE.kpairs = kpairs
+local _native_coroutine = coroutine
+local coroutine = setmetatable({}, {
+	__index = _native_coroutine,
+})
+_MODULE.coroutine = coroutine
+local _native_debug = debug
+local debug = setmetatable({}, {
+	__index = _native_debug,
+})
+_MODULE.debug = debug
+local _native_io = io
+local io = setmetatable({}, {
+	__index = _native_io,
+})
+_MODULE.io = io
+local _native_math = math
+local math = setmetatable({}, {
+	__index = _native_math,
+})
+_MODULE.math = math
+function math.clamp(x, min, max)
+	return math.min(math.max(x, min), max)
+end
+function math.product(...)
+	local multiplicands = { ... }
+	local result = 1
+	for i, multiplicand in ipairs(multiplicands) do
+		result = result * multiplicand
+	end
+	return result
+end
+function math.round(x)
+	return math.floor(x + 0.5)
+end
+function math.sum(...)
+	local summands = { ... }
+	local result = 0
+	for i, summand in ipairs(summands) do
+		result = result + summand
+	end
+	return result
+end
+local _native_os = os
+local os = setmetatable({}, {
+	__index = _native_os,
+})
+_MODULE.os = os
+local _native_package = package
+local package = setmetatable({}, {
+	__index = _native_package,
+})
+_MODULE.package = package
+local _native_string = string
+local string = setmetatable({}, {
+	__index = _native_string,
+})
+_MODULE.string = string
+function string.split(s, separator)
+	if separator == nil then
+		separator = "%s+"
+	end
+	local result = {}
+	local start, __ERDE_SUBSTITUTE_end__ = s:find(separator)
+	while start ~= nil do
+		table.insert(result, s:sub(1, start - 1))
+		s = s:sub(__ERDE_SUBSTITUTE_end__ + 1) or ""
+		start, __ERDE_SUBSTITUTE_end__ = s:find(separator)
+	end
+	table.insert(result, s)
+	return result
+end
+function string.escape(s)
+	local result = {}
+	for _, part in ipairs(string.split(s, "%%%%")) do
+		part = part:gsub("^([().*?[^$+-])", "%%%1")
+		part = part:gsub("([^%%])([().*?[^$+-])", "%1%%%2")
+		part = part:gsub("%%([^%%().*?[^$+-])", "%%%%%1")
+		part = part:gsub("%%$", "%%%%")
+		table.insert(result, part)
+	end
+	return table.concat(result, "%%")
+end
+function string.trim(s, pattern)
+	if pattern == nil then
+		pattern = "%s+"
+	end
+	return s:gsub(("^" .. tostring(pattern)), ""):gsub((tostring(pattern) .. "$"), "")
+end
+local _native_table = table
+local table = setmetatable({}, {
+	__index = _native_table,
+})
+_MODULE.table = table
+function table.assign(t, ...)
+	for _, _t in pairs({
+		...,
+	}) do
+		for key, value in pairs(_t) do
+			if type(key) == "string" then
+				t[key] = value
+			end
+		end
+	end
+end
+function table.clone(t)
+	local result = {}
+	for key, value in pairs(t) do
+		if type(value) == "table" then
+			result[key] = table.clone(value)
+		else
+			result[key] = value
+		end
+	end
+	return result
+end
+function table.copy(t)
+	local result = {}
+	for key, value in pairs(t) do
+		result[key] = value
+	end
+	return result
+end
+function table.default(t, ...)
+	for _, _t in pairs({
+		...,
+	}) do
+		for key, value in pairs(_t) do
+			if type(key) == "string" and t[key] == nil then
+				t[key] = value
+			end
+		end
+	end
+end
+function table.filter(t, callback)
+	local result = {}
+	for key, value in pairs(t) do
+		if callback(value, key) then
+			if type(key) == "number" then
+				table.insert(result, value)
+			else
+				result[key] = value
+			end
+		end
+	end
+	return result
+end
+function table.find(t, callback)
+	if type(callback) == "function" then
+		for key, value in pairs(t) do
+			if callback(value, key) then
+				return value, key
+			end
+		end
+	else
+		for key, value in pairs(t) do
+			if value == callback then
+				return value, key
+			end
+		end
+	end
+end
+function table.keys(t)
+	local result = {}
+	for key, value in pairs(t) do
+		table.insert(result, key)
+	end
+	return result
+end
+function table.map(t, callback)
+	local result = {}
+	for key, value in pairs(t) do
+		local newValue, newKey = callback(value, key)
+		if newKey ~= nil then
+			result[newKey] = newValue
+		elseif type(key) == "number" then
+			table.insert(result, newValue)
+		else
+			result[key] = newValue
+		end
+	end
+	return result
+end
+function table.reduce(t, result, callback)
+	for key, value in pairs(t) do
+		result = callback(result, value, key)
+	end
+	return result
+end
+function table.reverse(t)
+	local len = #t
+	for i = 1, math.floor(len / 2) do
+		t[i], t[len - i + 1] = t[len - i + 1], t[i]
+	end
+end
+function table.slice(t, start, __ERDE_SUBSTITUTE_end__)
+	if start == nil then
+		start = 0
+	end
+	if __ERDE_SUBSTITUTE_end__ == nil then
+		__ERDE_SUBSTITUTE_end__ = #t
+	end
+	local result, len = {}, #t
+	if start < 0 then
+		start = start + len + 1
+	end
+	if __ERDE_SUBSTITUTE_end__ < 0 then
+		__ERDE_SUBSTITUTE_end__ = __ERDE_SUBSTITUTE_end__ + len + 1
+	end
+	for i = math.max(start, 0), math.min(__ERDE_SUBSTITUTE_end__, len) do
+		table.insert(result, t[i])
+	end
+	return result
+end
+function table.values(t)
+	local result = {}
+	for key, value in pairs(t) do
+		table.insert(result, value)
+	end
+	return result
+end
+return _MODULE
+-- Compiled with Erde 0.6.0-1
+-- __ERDE_COMPILED__
---
+++ stdlib_spec.lua
@@ -0,0 +1,1054 @@
+local stdlib = require("stdlib")
+spec("load / unload", function()
+	assert.is_nil(kpairs)
+	assert.is_nil(math.clamp)
+	assert.is_nil(math.round)
+	assert.is_nil(table.assign)
+	assert.is_nil(table.clone)
+	stdlib.load()
+	assert.is_function(kpairs)
+	assert.is_function(math.clamp)
+	assert.is_function(math.round)
+	assert.is_function(table.assign)
+	assert.is_function(table.clone)
+	stdlib.unload()
+	assert.is_nil(kpairs)
+	assert.is_nil(math.clamp)
+	assert.is_nil(math.round)
+	assert.is_nil(table.assign)
+	assert.is_nil(table.clone)
+end)
+spec("default index to native math lib", function()
+	assert.is_function(stdlib.math.ceil)
+	assert.is_function(stdlib.math.floor)
+	assert.is_function(stdlib.math.min)
+	assert.is_function(stdlib.math.max)
+end)
+spec("clamp", function()
+	assert.are.equal(0, stdlib.math.clamp(0, 0, 1))
+	assert.are.equal(0.5, stdlib.math.clamp(0.5, 0, 1))
+	assert.are.equal(0, stdlib.math.clamp(-0.5, 0, 1))
+	assert.are.equal(1, stdlib.math.clamp(1.5, 0, 1))
+end)
+spec("product", function()
+	assert.are.equal(1, stdlib.math.product())
+	assert.are.equal(0, stdlib.math.product(0))
+	assert.are.equal(1, stdlib.math.product(1))
+	assert.are.equal(24, stdlib.math.product(1, 2, 3, 4))
+	assert.are.equal(-6, stdlib.math.product(1, -2, 3))
+end)
+spec("round", function()
+	assert.are.equal(-1, stdlib.math.round(-1))
+	assert.are.equal(-1, stdlib.math.round(-0.9))
+	assert.are.equal(-1, stdlib.math.round(-0.51))
+	assert.are.equal(0, stdlib.math.round(-0.5))
+	assert.are.equal(0, stdlib.math.round(-0.49))
+	assert.are.equal(0, stdlib.math.round(-0.1))
+	assert.are.equal(0, stdlib.math.round(0))
+	assert.are.equal(0, stdlib.math.round(0.1))
+	assert.are.equal(0, stdlib.math.round(0.49))
+	assert.are.equal(1, stdlib.math.round(0.5))
+	assert.are.equal(1, stdlib.math.round(0.51))
+	assert.are.equal(1, stdlib.math.round(0.9))
+	assert.are.equal(1, stdlib.math.round(1))
+end)
+spec("sum", function()
+	assert.are.equal(0, stdlib.math.sum())
+	assert.are.equal(0, stdlib.math.sum(0))
+	assert.are.equal(1, stdlib.math.sum(1))
+	assert.are.equal(10, stdlib.math.sum(1, 2, 3, 4))
+	assert.are.equal(-2, stdlib.math.sum(1, -2, 3, -4))
+end)
+spec("default index to native string lib", function()
+	assert.is_function(stdlib.string.byte)
+	assert.is_function(stdlib.string.char)
+	assert.is_function(stdlib.string.dump)
+end)
+spec("escape", function()
+	assert.are.equal("a", stdlib.string.escape("a"))
+	assert.are.equal("1", stdlib.string.escape("1"))
+	assert.are.equal(",", stdlib.string.escape(","))
+	assert.are.equal("%%", stdlib.string.escape("%"))
+	assert.are.equal("%(", stdlib.string.escape("("))
+	assert.are.equal("%)", stdlib.string.escape(")"))
+	assert.are.equal("%.", stdlib.string.escape("."))
+	assert.are.equal("%*", stdlib.string.escape("*"))
+	assert.are.equal("%?", stdlib.string.escape("?"))
+	assert.are.equal("%[", stdlib.string.escape("["))
+	assert.are.equal("%^", stdlib.string.escape("^"))
+	assert.are.equal("%$", stdlib.string.escape("$"))
+	assert.are.equal("%+", stdlib.string.escape("+"))
+	assert.are.equal("%-", stdlib.string.escape("-"))
+	assert.are.equal("a%%", stdlib.string.escape("a%"))
+	assert.are.equal("a%(", stdlib.string.escape("a("))
+	assert.are.equal("a%)", stdlib.string.escape("a)"))
+	assert.are.equal("a%.", stdlib.string.escape("a."))
+	assert.are.equal("a%*", stdlib.string.escape("a*"))
+	assert.are.equal("a%?", stdlib.string.escape("a?"))
+	assert.are.equal("a%[", stdlib.string.escape("a["))
+	assert.are.equal("a%^", stdlib.string.escape("a^"))
+	assert.are.equal("a%$", stdlib.string.escape("a$"))
+	assert.are.equal("a%+", stdlib.string.escape("a+"))
+	assert.are.equal("a%-", stdlib.string.escape("a-"))
+	assert.are.equal("%%", stdlib.string.escape("%%"))
+	assert.are.equal("%(", stdlib.string.escape("%("))
+	assert.are.equal("%)", stdlib.string.escape("%)"))
+	assert.are.equal("%.", stdlib.string.escape("%."))
+	assert.are.equal("%*", stdlib.string.escape("%*"))
+	assert.are.equal("%?", stdlib.string.escape("%?"))
+	assert.are.equal("%[", stdlib.string.escape("%["))
+	assert.are.equal("%^", stdlib.string.escape("%^"))
+	assert.are.equal("%$", stdlib.string.escape("%$"))
+	assert.are.equal("%+", stdlib.string.escape("%+"))
+	assert.are.equal("%-", stdlib.string.escape("%-"))
+	assert.are.equal("%%%(", stdlib.string.escape("%%("))
+	assert.are.equal("%(%%", stdlib.string.escape("(%%"))
+	assert.are.equal("%%%%", stdlib.string.escape("%%%%"))
+	assert.are.equal("%%%(%%", stdlib.string.escape("%%(%%"))
+end)
+spec("split", function()
+	assert.are.same({
+		"a",
+		"b",
+		"c",
+	}, stdlib.string.split("a b c"))
+	assert.are.same({
+		"a",
+		"b",
+		"c",
+	}, stdlib.string.split("a  b \tc"))
+	assert.are.same({
+		"",
+		"a",
+	}, stdlib.string.split(" a"))
+	assert.are.same({
+		"a",
+		"",
+	}, stdlib.string.split("a "))
+	assert.are.same({
+		"",
+		"a",
+		"",
+	}, stdlib.string.split(" a "))
+	assert.are.same({
+		" ",
+		" ",
+	}, stdlib.string.split(" a ", "%a+"))
+	assert.are.same({
+		"hello",
+		"world",
+	}, stdlib.string.split("hello11world", "%d+"))
+end)
+spec("trim", function()
+	assert.are.equal("hello", stdlib.string.trim("hello"))
+	assert.are.equal("hello", stdlib.string.trim("hello "))
+	assert.are.equal("hello", stdlib.string.trim(" hello"))
+	assert.are.equal("hello", stdlib.string.trim(" hello "))
+	assert.are.equal("hello", stdlib.string.trim("hello  "))
+	assert.are.equal("hello", stdlib.string.trim("  hello"))
+	assert.are.equal("hello", stdlib.string.trim("  hello  "))
+	assert.are.equal("hello", stdlib.string.trim("\thello"))
+	assert.are.equal("hello", stdlib.string.trim("hello\t"))
+	assert.are.equal("hello", stdlib.string.trim("\thello\t"))
+	assert.are.equal("hello", stdlib.string.trim("\t\thello"))
+	assert.are.equal("hello", stdlib.string.trim("hello\t\t"))
+	assert.are.equal("hello", stdlib.string.trim("\t\thello\t\t"))
+	assert.are.equal("hello", stdlib.string.trim("\nhello"))
+	assert.are.equal("hello", stdlib.string.trim("hello\n"))
+	assert.are.equal("hello", stdlib.string.trim("\nhello\n"))
+	assert.are.equal("hello", stdlib.string.trim("\n\nhello"))
+	assert.are.equal("hello", stdlib.string.trim("hello\n\n"))
+	assert.are.equal("hello", stdlib.string.trim("\n\nhello\n\n"))
+	assert.are.equal("hello", stdlib.string.trim("\n\t hello\t \n"))
+	assert.are.equal("hello", stdlib.string.trim("xxhelloxx", "x+"))
+end)
+local function array_sort(a, b)
+	if type(a) == type(b) then
+		return a < b
+	else
+		return type(a) == "number"
+	end
+end
+local function assert_array(expected, received)
+	table.sort(expected, array_sort)
+	table.sort(received, array_sort)
+	assert.are.same(expected, received)
+end
+spec("default index to native table lib", function()
+	assert.is_function(stdlib.table.insert)
+	assert.is_function(stdlib.table.remove)
+	assert.is_function(stdlib.table.concat)
+end)
+spec("assign", function()
+	function assert_assign(expected, target, ...)
+		stdlib.table.assign(target, ...)
+		assert.are.same(expected, target)
+	end
+	assert_assign({}, {})
+	assert_assign({
+		a = 1,
+	}, {}, {
+		a = 1,
+	})
+	assert_assign({
+		a = 2,
+	}, {}, {
+		a = 1,
+	}, {
+		a = 2,
+	})
+	assert_assign({
+		a = 1,
+		b = 2,
+	}, {}, {
+		a = 1,
+	}, {
+		b = 2,
+	})
+	assert_assign({
+		a = 2,
+	}, {
+		a = 1,
+	}, {
+		a = 2,
+	})
+	assert_assign({
+		a = 1,
+		b = 2,
+	}, {
+		a = 1,
+	}, {
+		b = 2,
+	})
+	assert_assign({
+		a = 1,
+		b = 2,
+		c = 3,
+	}, {
+		a = 1,
+		c = 3,
+	}, {
+		b = 2,
+	})
+	assert_assign({
+		1,
+	}, {
+		1,
+	})
+	assert_assign({}, {}, {
+		1,
+	})
+end)
+spec("clone", function()
+	function assert_clone(t, clone)
+		if clone == nil then
+			clone = stdlib.table.clone(t)
+		end
+		assert.are_not.equal(t, clone)
+		assert.are.same(t, clone)
+		for key, value in pairs(t) do
+			if type(value) == "stdlib.table" then
+				assert_clone(value, clone[key])
+			end
+		end
+	end
+	assert_clone({})
+	assert_clone({
+		1,
+		2,
+		3,
+	})
+	assert_clone({
+		a = 1,
+		b = 2,
+		c = 3,
+	})
+	assert_clone({
+		a = 1,
+		"hello",
+	})
+	assert_clone({
+		a = 1,
+		b = 2,
+		"hello",
+		"world",
+	})
+	assert_clone({
+		{
+			"hello",
+		},
+	})
+	assert_clone({
+		a = {
+			"hello",
+		},
+	})
+	assert_clone({
+		a = {
+			"hello",
+		},
+		{
+			"world",
+		},
+	})
+end)
+spec("copy", function()
+	function assert_copy(t)
+		local copy = stdlib.table.copy(t)
+		assert.are_not.equal(t, copy)
+		assert.are.same(t, copy)
+		for key, value in pairs(t) do
+			assert.are.equal(value, copy[key])
+		end
+	end
+	assert_copy({})
+	assert_copy({
+		1,
+		2,
+		3,
+	})
+	assert_copy({
+		a = 1,
+		b = 2,
+		c = 3,
+	})
+	assert_copy({
+		a = 1,
+		"hello",
+	})
+	assert_copy({
+		a = 1,
+		b = 2,
+		"hello",
+		"world",
+	})
+	assert_copy({
+		{
+			"hello",
+		},
+	})
+	assert_copy({
+		a = {
+			"hello",
+		},
+	})
+	assert_copy({
+		a = {
+			"hello",
+		},
+		{
+			"world",
+		},
+	})
+end)
+spec("default", function()
+	function assert_default(expected, target, ...)
+		stdlib.table.default(target, ...)
+		assert.are.same(expected, target)
+	end
+	assert_default({}, {})
+	assert_default({
+		a = 1,
+	}, {}, {
+		a = 1,
+	})
+	assert_default({
+		a = 1,
+	}, {}, {
+		a = 1,
+	}, {
+		a = 2,
+	})
+	assert_default({
+		a = 1,
+		b = 2,
+	}, {}, {
+		a = 1,
+	}, {
+		b = 2,
+	})
+	assert_default({
+		a = 1,
+	}, {
+		a = 1,
+	}, {
+		a = 2,
+	})
+	assert_default({
+		a = 1,
+		b = 2,
+	}, {
+		a = 1,
+	}, {
+		b = 2,
+	})
+	assert_default({
+		a = 1,
+		b = 2,
+		c = 3,
+	}, {
+		a = 1,
+		c = 3,
+	}, {
+		b = 2,
+	})
+	assert_default({
+		1,
+	}, {
+		1,
+	})
+	assert_default({}, {}, {
+		1,
+	})
+end)
+spec("filter", function()
+	assert.are.same(
+		{},
+		stdlib.table.filter({}, function()
+			return false
+		end)
+	)
+	assert.are.same(
+		{},
+		stdlib.table.filter({}, function()
+			return true
+		end)
+	)
+	assert.are.same(
+		{},
+		stdlib.table.filter({
+			1,
+			2,
+			3,
+		}, function()
+			return false
+		end)
+	)
+	assert.are.same(
+		{
+			1,
+			2,
+			3,
+		},
+		stdlib.table.filter({
+			1,
+			2,
+			3,
+		}, function()
+			return true
+		end)
+	)
+	assert.are.same(
+		{},
+		stdlib.table.filter({
+			a = 1,
+			b = 2,
+		}, function()
+			return false
+		end)
+	)
+	assert.are.same(
+		{
+			a = 1,
+			b = 2,
+		},
+		stdlib.table.filter({
+			a = 1,
+			b = 2,
+		}, function()
+			return true
+		end)
+	)
+	assert.are.same(
+		{
+			1,
+			2,
+		},
+		stdlib.table.filter({
+			1,
+			2,
+			3,
+		}, function(value)
+			return value < 3
+		end)
+	)
+	assert.are.same(
+		{
+			3,
+		},
+		stdlib.table.filter({
+			1,
+			2,
+			3,
+		}, function(value)
+			return value > 2
+		end)
+	)
+	assert.are.same(
+		{
+			a = 1,
+		},
+		stdlib.table.filter({
+			a = 1,
+			b = 2,
+		}, function(value)
+			return value < 2
+		end)
+	)
+	assert.are.same(
+		{
+			b = 2,
+		},
+		stdlib.table.filter({
+			a = 1,
+			b = 2,
+		}, function(value)
+			return value > 1
+		end)
+	)
+	assert.are.same(
+		{
+			a = 1,
+		},
+		stdlib.table.filter({
+			a = 1,
+			b = 2,
+		}, function(_, key)
+			return key == "a"
+		end)
+	)
+end)
+spec("find", function()
+	function assert_find(expected_key, t, callback)
+		local value, key = stdlib.table.find(t, callback)
+		assert.are.equal(expected_key, key)
+		assert.are.equal(t[expected_key], value)
+	end
+	assert_find(1, {
+		"a",
+		"b",
+		"c",
+	}, "a")
+	assert_find(2, {
+		"a",
+		"b",
+		"c",
+	}, "b")
+	assert_find(3, {
+		"a",
+		"b",
+		"c",
+	}, "c")
+	assert_find(nil, {
+		"a",
+		"b",
+		"c",
+	}, "d")
+	assert_find("a", {
+		a = "x",
+		b = "y",
+		c = "z",
+	}, "x")
+	assert_find("b", {
+		a = "x",
+		b = "y",
+		c = "z",
+	}, "y")
+	assert_find("c", {
+		a = "x",
+		b = "y",
+		c = "z",
+	}, "z")
+	assert_find(nil, {
+		a = "x",
+		b = "y",
+		c = "z",
+	}, "w")
+	assert_find(1, {
+		30,
+		20,
+		10,
+	}, function(value)
+		return value < 40
+	end)
+	assert_find(2, {
+		30,
+		20,
+		10,
+	}, function(value)
+		return value < 30
+	end)
+	assert_find(3, {
+		30,
+		20,
+		10,
+	}, function(value)
+		return value < 20
+	end)
+	assert_find("a", {
+		a = 10,
+	}, function(value)
+		return value < 20
+	end)
+	assert_find(nil, {
+		a = 20,
+	}, function(value)
+		return value < 20
+	end)
+end)
+spec("keys", function()
+	assert_array({}, stdlib.table.keys({}))
+	assert_array(
+		{
+			1,
+		},
+		stdlib.table.keys({
+			"a",
+		})
+	)
+	assert_array(
+		{
+			1,
+			2,
+		},
+		stdlib.table.keys({
+			"a",
+			"b",
+		})
+	)
+	assert_array(
+		{
+			"a",
+		},
+		stdlib.table.keys({
+			a = 10,
+		})
+	)
+	assert_array(
+		{
+			"a",
+			"b",
+		},
+		stdlib.table.keys({
+			a = 10,
+			b = 20,
+		})
+	)
+	assert_array(
+		{
+			1,
+			"b",
+		},
+		stdlib.table.keys({
+			"a",
+			b = 10,
+		})
+	)
+end)
+spec("map", function()
+	assert.are.same(
+		{},
+		stdlib.table.map({}, function(value)
+			return true
+		end)
+	)
+	assert.are.same(
+		{},
+		stdlib.table.map({
+			1,
+			2,
+			3,
+		}, function(value)
+			return nil
+		end)
+	)
+	assert.are.same(
+		{
+			0,
+			10,
+			20,
+		},
+		stdlib.table.map({
+			0,
+			1,
+			2,
+		}, function(value)
+			return 10 * value
+		end)
+	)
+	assert.are.same(
+		{
+			a = 10,
+			b = 20,
+		},
+		stdlib.table.map({
+			a = 1,
+			b = 2,
+		}, function(value)
+			return 10 * value
+		end)
+	)
+	assert.are.same(
+		{
+			a = 10,
+		},
+		stdlib.table.map({
+			1,
+		}, function(value)
+			return 10 * value, "a"
+		end)
+	)
+	assert.are.same(
+		{
+			10,
+		},
+		stdlib.table.map({
+			a = 1,
+		}, function(value)
+			return 10 * value, 1
+		end)
+	)
+	assert.are.same(
+		{
+			b = 10,
+		},
+		stdlib.table.map({
+			a = 10,
+		}, function(value)
+			return value, "b"
+		end)
+	)
+end)
+spec("reduce", function()
+	assert.are.equal(
+		60,
+		stdlib.table.reduce(
+			{
+				10,
+				20,
+				30,
+			},
+			0,
+			function(reduction, value)
+				return reduction + value
+			end
+		)
+	)
+	assert.are.equal(
+		70,
+		stdlib.table.reduce(
+			{
+				10,
+				20,
+				30,
+			},
+			10,
+			function(reduction, value)
+				return reduction + value
+			end
+		)
+	)
+	assert.are.equal(
+		60,
+		stdlib.table.reduce(
+			{
+				a = 10,
+				b = 20,
+				c = 30,
+			},
+			0,
+			function(reduction, value)
+				return reduction + value
+			end
+		)
+	)
+	assert.are.equal(
+		70,
+		stdlib.table.reduce(
+			{
+				a = 10,
+				b = 20,
+				c = 30,
+			},
+			10,
+			function(reduction, value)
+				return reduction + value
+			end
+		)
+	)
+end)
+spec("reverse", function()
+	function assert_reverse(expected, t)
+		stdlib.table.reverse(t)
+		assert.are.same(expected, t)
+	end
+	assert_reverse({}, {})
+	assert_reverse({
+		"a",
+	}, {
+		"a",
+	})
+	assert_reverse({
+		"b",
+		"a",
+	}, {
+		"a",
+		"b",
+	})
+	assert_reverse({
+		"c",
+		"b",
+		"a",
+	}, {
+		"a",
+		"b",
+		"c",
+	})
+	assert_reverse({
+		"c",
+		"b",
+		"a",
+		d = true,
+	}, {
+		"a",
+		"b",
+		"c",
+		d = true,
+	})
+end)
+spec("slice", function()
+	assert.are.same({}, stdlib.table.slice({}))
+	assert.are.same(
+		{
+			1,
+			2,
+		},
+		stdlib.table.slice({
+			1,
+			2,
+		})
+	)
+	assert.are.same(
+		{
+			1,
+			2,
+		},
+		stdlib.table.slice({
+			1,
+			2,
+			a = 3,
+		})
+	)
+	assert.are.same(
+		{
+			"a",
+			"b",
+			"c",
+		},
+		stdlib.table.slice({
+			"a",
+			"b",
+			"c",
+		}, 1)
+	)
+	assert.are.same(
+		{
+			"b",
+			"c",
+		},
+		stdlib.table.slice({
+			"a",
+			"b",
+			"c",
+		}, 2)
+	)
+	assert.are.same(
+		{
+			"c",
+		},
+		stdlib.table.slice({
+			"a",
+			"b",
+			"c",
+		}, 3)
+	)
+	assert.are.same(
+		{
+			"c",
+		},
+		stdlib.table.slice({
+			"a",
+			"b",
+			"c",
+		}, -1)
+	)
+	assert.are.same(
+		{
+			"b",
+			"c",
+		},
+		stdlib.table.slice({
+			"a",
+			"b",
+			"c",
+		}, -2)
+	)
+	assert.are.same(
+		{
+			"a",
+			"b",
+			"c",
+		},
+		stdlib.table.slice({
+			"a",
+			"b",
+			"c",
+		}, -3)
+	)
+	assert.are.same(
+		{
+			"a",
+		},
+		stdlib.table.slice({
+			"a",
+			"b",
+			"c",
+		}, 1, 1)
+	)
+	assert.are.same(
+		{
+			"a",
+			"b",
+		},
+		stdlib.table.slice({
+			"a",
+			"b",
+			"c",
+		}, 1, 2)
+	)
+	assert.are.same(
+		{
+			"b",
+		},
+		stdlib.table.slice({
+			"a",
+			"b",
+			"c",
+		}, 2, 2)
+	)
+	assert.are.same(
+		{
+			"a",
+			"b",
+			"c",
+		},
+		stdlib.table.slice({
+			"a",
+			"b",
+			"c",
+		}, 1, 3)
+	)
+	assert.are.same(
+		{
+			"b",
+			"c",
+		},
+		stdlib.table.slice({
+			"a",
+			"b",
+			"c",
+		}, 2, -1)
+	)
+	assert.are.same(
+		{
+			"a",
+			"b",
+			"c",
+		},
+		stdlib.table.slice({
+			"a",
+			"b",
+			"c",
+		}, 1, -1)
+	)
+	assert.are.same(
+		{
+			"c",
+		},
+		stdlib.table.slice({
+			"a",
+			"b",
+			"c",
+		}, 3, -1)
+	)
+	assert.are.same(
+		{
+			"b",
+			"c",
+		},
+		stdlib.table.slice({
+			"a",
+			"b",
+			"c",
+		}, -2, 3)
+	)
+	assert.are.same(
+		{},
+		stdlib.table.slice({
+			"a",
+			"b",
+			"c",
+		}, 4)
+	)
+end)
+spec("values", function()
+	assert_array({}, stdlib.table.values({}))
+	assert_array(
+		{
+			"a",
+		},
+		stdlib.table.values({
+			"a",
+		})
+	)
+	assert_array(
+		{
+			"a",
+			"b",
+		},
+		stdlib.table.values({
+			"a",
+			"b",
+		})
+	)
+	assert_array(
+		{
+			1,
+		},
+		stdlib.table.values({
+			a = 1,
+		})
+	)
+	assert_array(
+		{
+			1,
+			2,
+		},
+		stdlib.table.values({
+			a = 1,
+			b = 2,
+		})
+	)
+	assert_array(
+		{
+			1,
+			"a",
+		},
+		stdlib.table.values({
+			"a",
+			b = 1,
+		})
+	)
+end)
+-- Compiled with Erde 0.6.0-1
+-- __ERDE_COMPILED__
